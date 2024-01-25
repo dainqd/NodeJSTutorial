2dad2aa191a5966394d4f8262b633e579a97b9c6
dainqd <ngodaix5tp@gmail.com>
2024-01-25 17:12:25 +0700

    dev.

--- app/server/app.js
+++ app/server/app.js
@@ -3,11 +3,11 @@ const cors = require('cors');
 
 const app = express();
 
-// var corsOptions = {
-//     origin: "https://localhost:3000"
-// };
-//
-// app.use(cors(corsOptions));
+var corsOptions = {
+    origin: 'http://localhost:3000'
+};
+
+app.use(cors(corsOptions));
 
 /* parse requests of content-type - application/json */
 app.use(express.json());
@@ -15,7 +15,7 @@ app.use(express.json());
 /* parse requests of content-type - application/x-www-form-urlencoded */
 app.use(express.urlencoded({ extended: true }));
 
-require("./routers/router")(app);
+require('./routers/router')(app);
 
 /* set port, listen for requests */
 const PORT = process.env.PORT || 3000;
